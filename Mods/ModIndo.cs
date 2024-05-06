59fea329170e744619b20f5628ab5b7efe86f68a
PinksSigmaMenu <166714996+PinksSigmaMenu@users.noreply.github.com>
2024-05-06 13:06:21 -0500

    Update ModIndo.cs

--- Mods/ModIndo.cs
+++ Mods/ModIndo.cs
@@ -14,9 +14,9 @@ namespace StupidTemplate.Mods
             GameObject signPosition = GameObject.Find("Environment Objects/LocalObjects_Prefab/City/CosmeticsRoomAnchor/outerstores/Bottom Floor/OutsideBuildings/InfoAnchor/infosign");
 
             GameObject textObject = new GameObject("TextObject");
-            textObject.transform.position = signPosition.transform.position + Vector3.up * 0.1f; 
+            textObject.transform.position = signPosition.transform.position + Vector3.up * 0.10f; 
             textObject.transform.rotation = signPosition.transform.rotation * Quaternion.Euler(0f, 90f, 0f); 
-            textObject.transform.localScale = new Vector3(0.02f, 0.20f, 0.02f);
+            textObject.transform.localScale = new Vector3(0.20f, 0.20f, 0.20f);
 
             textObject.transform.Rotate(Vector3.up, 180f);
 
